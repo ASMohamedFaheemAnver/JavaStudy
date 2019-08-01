ed492ef652f2d8439989a913320d9954f4e0ae6c
ubunturifa <jstrfaheem065@gmail.com>
2019-08-01 19:31:04 +0530

    Added DiningPhilosophers,

--- src/com/semaphores/DiningPhilosophers.java
+++ src/com/semaphores/DiningPhilosophers.java
@@ -13,13 +13,13 @@ public class DiningPhilosophers {
             for(int i = 0; i<forks.length; i++){
                 Object leftFork = forks[i];
                 Object rightFork = forks[(i+1)%philosophers.length];
-                if(i!=(philosophers.length -1)){
+                // if(i!=(philosophers.length -1)){
                     philosophers[i] = new Philosopher(leftFork, rightFork);
 
 
-                }else{
+                /*}else{
                     philosophers[i] = new Philosopher(rightFork, leftFork);
-                }
+                }*/
                 Thread t = new Thread(philosophers[i], "Philosopher " + (i+1));
                 t.start();
             }
