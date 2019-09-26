326c88a020c4341d67ef7bc66bbc640cd3132ea1
ubunturifa <jstrfaheem065@gmail.com>
2019-09-27 01:55:28 +0530

    Observer pattern,

---
+++ src/com/extra/observerpattern/ObservableDemo.java
@@ -0,0 +1,26 @@
+package com.extra.observerpattern;
+
+import java.util.Observable;
+import java.util.concurrent.TimeUnit;
+
+public class ObservableDemo extends Observable {
+    private String weather;
+    public ObservableDemo(String weather){
+        this.weather = weather;
+    }
+
+    public String getWeather(){
+        return weather;
+    }
+
+    public void setWeather(String weather){
+        this.weather = weather;
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setChanged();
+        notifyObservers();
+    }
+}
---
+++ src/com/extra/observerpattern/ObserverOneDemo.java
@@ -0,0 +1,26 @@
+package com.extra.observerpattern;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObserverOneDemo implements Observer {
+
+    private static ObservableDemo weatherUpdate;
+
+    @Override
+    public void update(Observable observable, Object o) {
+        weatherUpdate = (ObservableDemo) observable;
+    }
+
+    public static void main(String[] args){
+        ObservableDemo observableDemo = new ObservableDemo(null);
+        observableDemo.addObserver(new ObserverOneDemo());
+        observableDemo.setWeather("Sunny");
+        System.out.println(weatherUpdate.getWeather());
+        System.out.println("Testing");
+        System.out.println("Testing");
+        System.out.println("Testing");
+        System.out.println("Testing");
+        System.out.println("Testing");
+    }
+}
