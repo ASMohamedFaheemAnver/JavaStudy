248e27ca99a4b258c8c5910f6c2d1863822fd290
ubunturifa <jstrfaheem065@gmail.com>
2019-10-20 00:30:55 +0530

    Deleted .class files,

--- src/com/acescoders/RandomGroup.java
+++ src/com/acescoders/RandomGroup.java
@@ -5,34 +5,37 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class RandomGroup {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
-        int numberofsequance = scanner.nextInt();
-        int numberofcard = scanner.nextInt();
-        int numberofgroup = scanner.nextInt();
-        scanner.nextLine();
-        for(int i = 0; i<numberofsequance; i++){
-            int generatedgroup = 0;
-            Map<Integer, Integer> newMap = new HashMap<Integer, Integer>();
-            for(int j = 0; j<numberofcard; j++){
-                int card = scanner.nextInt();
-                if (newMap.containsKey(card)){
-                    newMap.put(card, newMap.get(card)+1);
-                }else {
-                    newMap.put(card, 1);
+
+        int numberOfSequence = scanner.nextInt();
+        int numberOfInput = scanner.nextInt();
+        int numberOfPairNeeded = scanner.nextInt();
+
+        for (int i = 0; i < numberOfSequence; i++) { // O(n)
+            Map<Integer, Integer> map = new HashMap<>();
+            int generatedPairs = 0;
+            boolean isBreak = false;
+            for (int j = 0; j < numberOfInput; j++) { // O(n)
+                int input = scanner.nextInt();
+
+                if (map.containsKey(input)) { // O(n) doubt
+                    int numberOfMembers = map.get(input);
+                    if (numberOfMembers < 2) {
+                        map.put(input, numberOfMembers + 1);
+                        generatedPairs++;
+                    } else if (numberOfMembers > 2) {
+                        scanner.nextLine();
+                        isBreak =true;
+                        break;
+                    }
+                } else {
+                    map.put(input, 1);
                 }
             }
-            int notsafeexist = 0;
-            for(int key : newMap.keySet()){
-                if(newMap.get(key)==2){
-                    generatedgroup++;
-                }else if(newMap.get(key)>2){
-                    notsafeexist++;
-                }
-            }
-            if(generatedgroup==numberofgroup && notsafeexist == 0){
+            if ((!isBreak)&&(generatedPairs == numberOfPairNeeded)) {
                 System.out.println("SAFE");
-            }else{
+            } else {
                 System.out.println("NOT SAFE");
             }
         }
