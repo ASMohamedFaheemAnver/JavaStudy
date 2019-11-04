c6fe8d774a2826f8e0ac312e776c3a57781f284a
ubunturifa <jstrfaheem065@gmail.com>
2019-11-04 16:26:57 +0530

    # MultiplicationTable,

--- src/com/rank/shecode/MultiplicationTable.java
+++ src/com/rank/shecode/MultiplicationTable.java
@@ -1,16 +1,41 @@
 package com.rank.shecode;
 
-import java.text.DecimalFormat;
-import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.IOException;
 
 public class MultiplicationTable {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        DecimalFormat decimalFormat = new DecimalFormat("00");
-        for (int i = 0; i<12; i++){
+    private static int bufferPointer = 0;
+    private static int bytesRead = 0;
+    private static int BUFFER_SIZE = 1<<16;
+    private static byte[] buffer = new byte[BUFFER_SIZE];
+    private static DataInputStream dataInputStream;
 
-            System.out.println(number + " x " + decimalFormat.format((i+1)) + " = " + (number*(i+1)));
+    public static byte read() throws IOException {
+        if (bufferPointer==bytesRead){
+            bytesRead = dataInputStream.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead==-1){
+                buffer[0] = -1;
+            }
+        }
+        return buffer[bufferPointer++];
+    }
+    public static void main(String[] args) throws IOException {
+        dataInputStream = new DataInputStream(System.in);
+        short input = 0;
+        byte c = read();
+        while (c <= ' ')
+            c = read();
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+        do {
+            input = (short) (input * 10 + c - '0');
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (input>=1&&input<=1000){
+            for (int i = 0; i<12; i++){
+                System.out.printf("%d x %02d = %d\n",input, i+1, (input*(i+1)));
+            }
         }
     }
 }
