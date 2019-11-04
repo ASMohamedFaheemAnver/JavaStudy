2b4240e798ab0be24c9244bcae87a66b505836f0
ubunturifa <jstrfaheem065@gmail.com>
2019-11-04 23:10:22 +0530

    I/O time with ReaderClass,

--- src/com/timecomplexity/fastestio/UsingReader.java
+++ src/com/timecomplexity/fastestio/UsingReader.java
@@ -3,39 +3,47 @@ package com.timecomplexity.fastestio;
 import java.io.DataInputStream;
 import java.io.IOException;
 
-public class UsingReader {
-    private static int bufferPointer = 0;
-    private static int bytesRead = 0;
-    private static int BUFFER_SIZE = 1<<16;
-    private static byte[] buffer = new byte[BUFFER_SIZE];
-    private static DataInputStream dataInputStream;
+class KeyBordBufferReader {
 
-    public static byte read() throws IOException {
-        if (bufferPointer==bytesRead){
-            bytesRead = dataInputStream.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-            if (bytesRead==-1){
-                buffer[0] = -1;
-            }
+    // Size of the buffer we are using
+    // We are using this buffer size to get 1..16*0 buffer at once
+    private final int BUFFER_SIZE = 1 << 16;
+
+    // We use DataInputStream to get data from the keyBord buffer
+    private DataInputStream dataInputStream = new DataInputStream(System.in);
+
+    // We are getting input as a byte
+    private byte[] buffer = new byte[BUFFER_SIZE];
+
+    // We use bufferPointer to get the current position of the buffer
+    private int bufferPointer = 0;
+
+    // We use bytesRead to remember how many bytes we read from the buffer
+    private int bytesRead = 0;
+
+    public void fillBuffer() throws IOException {
+        bytesRead = dataInputStream.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+        if (bytesRead==-1){
+            buffer[0] = -1;
         }
+    }
+
+    // We use read method to get&return bytes from buffer
+    public byte read() throws IOException {
+        if (bufferPointer==bytesRead){
+            fillBuffer();
+        }
+        // This will return one element from the buffer
         return buffer[bufferPointer++];
     }
-    public static void main(String[] args) throws IOException {
-        dataInputStream = new DataInputStream(System.in);
-        short input = 0;
-        byte c = read();
-        while (c <= ' ')
-            c = read();
-        boolean neg = (c == '-');
-        if (neg)
-            c = read();
-        do {
-            input = (short) (input * 10 + c - '0');
-        } while ((c = read()) >= '0' && c <= '9');
+}
 
-        if (input>=1&&input<=1000){
-            for (int i = 0; i<12; i++){
-                System.out.printf("%d x %02d = %d\n",input, i+1, (input*(i+1)));
-            }
+
+public class UsingReader{
+    public static void main(String[] args) throws IOException {
+        KeyBordBufferReader keyBordBufferReader = new KeyBordBufferReader();
+        for (int i = 0; i<5; i++){
+            System.out.println((char) keyBordBufferReader.read());
         }
     }
 }
