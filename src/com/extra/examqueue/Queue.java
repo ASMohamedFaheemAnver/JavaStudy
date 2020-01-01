9e62cc5a1356aba0c173a8f5817c0a39b2a517e8
ubunturifa <jstrfaheem065@gmail.com>
2020-01-01 22:30:24 +0530

    Full finished,

--- src/com/extra/examqueue/Queue.java
+++ src/com/extra/examqueue/Queue.java
@@ -1,5 +1,7 @@
 package com.extra.examqueue;
 
+import java.io.*;
+
 class PriorityElement{
     private int priority;
     private String data;
@@ -33,7 +35,7 @@ class Queue {
         }else if(!isFull){
             boolean isAdded = false;
             for (int i = 0; i<count; i++){
-                if(objects[i].getPriority()<newObject.getPriority()){
+                if(objects[i].getPriority()>newObject.getPriority()){
                     for (int j = count; j>i; j--){
                         objects[j] = objects[j-1];
                     }
@@ -52,20 +54,86 @@ class Queue {
         }
     }
 
-    public static void main(String[] args){
-        PriorityElement one = new PriorityElement(1, "1");
-        PriorityElement two = new PriorityElement(2, "2");
-        PriorityElement three = new PriorityElement(0, "3");
-        Queue queue = new Queue(5);
-        queue.insert(one);
-        queue.insert(two);
-        queue.insert(three);
-        queue.insert(one);
-        queue.insert(two);
-        queue.insert(three);
-        queue.insert(one);
-        queue.insert(two);
-        queue.insert(three);
-        System.out.printf("Testring");
+    public PriorityElement remove(){
+        PriorityElement front = null;
+        if (!isEmpty()){
+            front = objects[0];
+            for (int i = 0; i<count-1; i++){
+                objects[i] = objects[i+1];
+            }
+            objects[--count] = null;
+        }
+        return front;
+    }
+
+    public boolean isEmpty(){
+        return count!=0 ? false : true;
+    }
+
+    public boolean isFull(){
+        return isFull;
+    }
+}
+
+class PriorityQueue extends Queue{
+    public PriorityQueue(int maxSizeOfQueue) {
+        super(maxSizeOfQueue);
+    }
+
+    public void insert(int priority, String data){
+        PriorityElement newObject = new PriorityElement(priority, data);
+        insert(newObject);
+    }
+}
+
+class WeatherReporter{
+    String[] readWeather() throws IOException {
+        FileReader inputFile = new FileReader("/home/ubunturifa/Desktop/GitHub/JavaStudy/src/com/extra/examqueue/weather.txt");
+        Reader reader = new BufferedReader(inputFile);
+
+        /*StreamTokenizer tokens = new StreamTokenizer(reader);
+        String data = "";
+        int chunk;
+        while ((chunk=tokens.nextToken())!=StreamTokenizer.TT_EOF){
+            switch (chunk){
+                case StreamTokenizer.TT_NUMBER:
+                    data += tokens.nval;
+                    break;
+                case  StreamTokenizer.TT_WORD:
+                    data += tokens.toString();
+            }
+        }*/
+
+        int chunk;
+        int count = 0;
+        String[] data = new String[10];
+        for (int i = 0; i<data.length; i++){
+            data[i] = "";
+        }
+        while ((chunk=reader.read())!=-1){
+            if (chunk!=10){
+                data[count] += (char)chunk;
+            }else {
+                count++;
+            }
+        }
+
+        return data;
+    }
+}
+
+class Implementer{
+    public static void main(String[] args) throws IOException {
+        WeatherReporter weatherReporter = new WeatherReporter();
+        String[] data = weatherReporter.readWeather();
+        PriorityQueue priorityQueue = new PriorityQueue(data.length);
+
+        for (String oneData: data){
+            String[] weatherData = oneData.split(" ");
+            priorityQueue.insert(Integer.parseInt(weatherData[2]), weatherData[0] + " " + weatherData[1]);
+        }
+        
+        PriorityElement least = priorityQueue.remove();
+        System.out.printf("Lowest temperature is in %s %d", least.getData(), least.getPriority());
     }
 }
