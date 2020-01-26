e1db65aa9191930b93a11b9949845ce5bcc3bd8b
ubunturifa <jstrfaheem065@gmail.com>
2020-01-26 11:16:31 +0530

    Builder pattern,

---
+++ src/com/designingpattern/builder/RobotBuilder.java
@@ -0,0 +1,15 @@
+package com.designingpattern.builder;
+
+public interface RobotBuilder {
+
+    public void buildRobotHead();
+
+    public void buildRobotTorso();
+
+    public void buildRobotArms();
+
+    public void buildRobotLegs();
+
+    public Robot getRobot();
+
+}
